755d9edd7cf63018f1fa63fe42c924100b104a73
Andres Ramirez <andresaaramirez@aol.com>
2020-07-06 13:50:08 -0400

    i added a query selector generatePassword

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+document.getElementById('password').addEventListener("click", generate);
+
+function generate() {
+  document.getElementById('password').innerHTML = generatePassword();
+}
+
 function generatePassword(){ 
   //stored characters are turned into arrays
-  let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split(''),
-  lowercase = 'abcdefghijklmnopqrstuvwxyz'.split(''),
-  numbers = '0123456789'.split(''),
-  specials = '!@#$%^&*()-={}~`<>?'.split('');
+  let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  lowercase = 'abcdefghijklmnopqrstuvwxyz',
+  numbers = '0123456789',
+  specials = '!@#$%^&*()-={}~`<>?';
   
   //initialize password array
   passwordArray = [];
@@ -14,9 +20,6 @@ function generatePassword(){
   //variable holds random generated number from string
   let randomNum2;
 
-  //password will combine all arrays into one and be final result
-  let finalPassword;
-
   //loop through the array given
   for (let i = 0; i < 12; i++) {
     randomNum = Math.floor(Math.random() * 4);
@@ -24,24 +27,21 @@ function generatePassword(){
     //if else statement will determine which numbers are push through
     if(randomNum == 0) {
       randomNum2 = Math.floor(Math.random() * 12);
-      passwordArray.push(uppercase[randomNumber2]);
+      passwordArray.push(uppercase[randomNum2]);
 
     } else if (randomNum == 1) {
       randomNum2 = Math.floor(Math.random() * 12);
-      passwordArray.push(lowercase[randomNumber2]);
+      passwordArray.push(lowercase[randomNum2]);
 
-    } else if (randomNumber2 == 2) {
+    } else if (randomNum == 2) {
       randomNumber2 = Math.floor(Math.random() * 12);
       passwordArray.push(numbers[randomNum2]);
-      conso
 
-    } else if (randomNumber2 == 3) {
+    } else if (randomNum == 3) {
       randomNumber2 = Math.floor(Math.random() * 12);
       passwordArray.push(specials[randomNum2]);
     }
-
   }
-  finalPassword = passwordArray.push(numbers, lowercase, uppercase, specials).join('');
-  console.log(finalPassword)
-};
-
+  document.querySelector("#password").innerHTML = passwordArray.join('');
+}
+generatePassword();
