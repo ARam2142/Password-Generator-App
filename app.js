bfdb9c0f00e21929bdf4876680c78301fba29252
Andres Ramirez <andresaaramirez@aol.com>
2020-07-06 01:21:08 -0400

    i split all variables into arrays

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
-//get elements from html
-const textarea = document.getElementById("password");
+//select button from html
+const generateBtn = document.querySelector('#generate');
 
-//store characters in variables
-let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#!$%^&*()_+?<>,.=-~`{}";
+//element to display password
+const displayPassword = document.querySelector('#password');
 
-button.onclick = ()=> {
-  let password = '';
+function generatePassword(){
+  //stored values
+  let uppercase= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  let numbers = '0123456789'
+  let specials = '!@#$%^&*()-={}~`<>?';
+
+  //values put into arrays
+  uppercaseSplit = uppercase.split('');
+  lowercaseSplit = lowercase.split('');
+  numbersSplit = numbers.split('');
+  specials = numbers.split('');
+  console.log(numbers.split(''))
+  array = [];
+
+
+  let password = ''
+
   for (let i = 0; i < 12; i++) {
-    password += password.charAt(Math.floor(Math.round() * characters.length));
+    password += values.charAt(Math.floor(Math.random() * values.length))
   }
-}
-textarea.value = password;
 
+}
