b288c2e87ca70c38c249049acb9066dff6fa6d00
Andres Ramirez <andresaaramirez@aol.com>
2020-07-06 22:38:19 -0400

    added a comment by case1

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-document.getElementById('password').addEventListener("click", generate);
-
-function generate() {
-  document.getElementById('password').innerHTML = generatePassword();
-}
-
 function generatePassword(){ 
-  //stored characters are turned into arrays
-  let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  lowercase = 'abcdefghijklmnopqrstuvwxyz',
-  numbers = '0123456789',
-  specials = '!@#$%^&*()-={}~`<>?';
+  //stored characters
+  let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let lowercase = 'abcdefghijklmnopqrstuvwxyz';
+  let numbers = '0123456789';
+  let specials = '!@#$%^&*()-={}~`<>?';
   
   //initialize password array
   passwordArray = [];
 
-  //variable holds random generated number from 4 variables
+  //variable holds random generated element from array
   let randomNum;
 
   //variable holds random generated number from string
@@ -24,24 +18,37 @@ function generatePassword(){
   for (let i = 0; i < 12; i++) {
     randomNum = Math.floor(Math.random() * 4);
 
-    //if else statement will determine which numbers are push through
-    if(randomNum == 0) {
-      randomNum2 = Math.floor(Math.random() * 12);
-      passwordArray.push(uppercase[randomNum2]);
+    //switch statement will determine which numbers are push through
+      switch(randomNum) {
+        case 1:
+        randomNum2 = Math.floor(Math.random() * 12);//random 12 characters are generated and held in randomNum2 variable
+        passwordArray.push(uppercase[randomNum2]);
+        break;
 
-    } else if (randomNum == 1) {
-      randomNum2 = Math.floor(Math.random() * 12);
-      passwordArray.push(lowercase[randomNum2]);
+        case 2:
+        randomNum2 = Math.floor(Math.random() * 12);
+        passwordArray.push(lowercase[randomNum2]);
+        break;
 
-    } else if (randomNum == 2) {
-      randomNumber2 = Math.floor(Math.random() * 12);
-      passwordArray.push(numbers[randomNum2]);
+        case 3: 
+        randomNumber2 = Math.floor(Math.random() * 12);
+        passwordArray.push(numbers[randomNum2]);
+        break;
 
-    } else if (randomNum == 3) {
-      randomNumber2 = Math.floor(Math.random() * 12);
-      passwordArray.push(specials[randomNum2]);
+        case 4: 
+        randomNumber2 = Math.floor(Math.random() * 12);
+        passwordArray.push(specials[randomNum2]);
+        default:
+      }
     }
-  }
-  document.querySelector("#password").innerHTML = passwordArray.join('');
 }
-generatePassword();
+
+var button = document.querySelector('#password');
+    button.addEventListener('click', function(e){
+    e.target.style.background = 'green';
+});
+
+//default
+function syncWindow(e) {
+  e.preventDefault();
+}
