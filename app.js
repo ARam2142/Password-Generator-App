2dab55f61c2ee909beacf38cb03a2083ba9ea1c5
Andres Ramirez <andresaaramirez@aol.com>
2020-07-12 22:43:55 -0400

    i have aadded more comments

--- app.js
+++ app.js
@@ -10,17 +10,14 @@ const inputUpper = document.getElementById('uppercheck');
 const inputLower = document.getElementById('lowercheck');
 const inputSymbol = document.getElementById('symcheck');
 const inputNumber = document.getElementById('numcheck');
-const inputLength = document.getElementById('passwordlength');
-
+const inputLength = document.getElementById('passwordlength').value;
+  
 function generatePassword(){ 
   //stored characters
   let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   let lowercase = 'abcdefghijklmnopqrstuvwxyz';
   let numbers = '0123456789';
   let specials = '!@#$%^&*()-={}~`<>?';
-
-  //set password length
-  let passlength = ''
 
   //initialize password array
   let passwordArray = [];
@@ -35,6 +32,7 @@ function generatePassword(){
   for (let i = 0; i < 10; i++) {
     randomNum = Math.floor(Math.random() * 4);//generate random character from 4 variab
       
+        //each button that is checked will generate a random set of characters
         if(inputUpper.checked) {
           randomNum2 = Math.floor(Math.random() * 21);
           passwordArray.push(uppercase[randomNum2]);
@@ -53,13 +51,20 @@ function generatePassword(){
         }
       }
     
-    //Now we have a password Array, we need to make this into a stirng and get it in the DOM
-    const button = document.querySelector('#displayPassword');
-    button.innerText = passwordArray.join('');
-    
+      //Now we have a password Array, we need to make this into a stirng and get it in the DOM
+      const button = document.querySelector('#displayPassword');
+      button.innerText = passwordArray.join('');
+
+      /*if (document.getElementById('passwordlength').value.length < "8" || document.getElementById('passwordlength').value.length > "128") {
+        alert('Please make sure your password is more than 8 characters and less than 128 characters');
+        return false;
+      } else {
+        return true;
+      }*/
 }
 
-genButton = document.querySelector('.click');
+//event listener that will fire the click event on the generate button
+var genButton = document.querySelector('.click');
 genButton.addEventListener('click', function(e){
      e.preventDefault();
      generatePassword();
