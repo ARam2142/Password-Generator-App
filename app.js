1f143e87f4b1e7683fff1be73b4cebe555057449
Andres Ramirez <andresaaramirez@aol.com>
2020-07-12 23:10:31 -0400

    i deleted unused code from js

--- app.js
+++ app.js
@@ -55,12 +55,16 @@ function generatePassword(){
       const button = document.querySelector('#displayPassword');
       button.innerText = passwordArray.join('');
 
-      /*if (document.getElementById('passwordlength').value.length < "8" || document.getElementById('passwordlength').value.length > "128") {
+      if (document.getElementById('passwordlength').value.length < "8" || document.getElementById('passwordlength').value.length > "128") {
         alert('Please make sure your password is more than 8 characters and less than 128 characters');
         return false;
       } else {
         return true;
-      }*/
+      }
+      if (document.getElementById('passwordlength').value == "" || document.getElementById('passwordlength').value < "8" || document.getElementById('passwordlength').value > "128") {
+        alert("Please introduce a valid length value! (6-50 characters)");
+        return;
+      }
 }
 
 //event listener that will fire the click event on the generate button
@@ -69,77 +73,3 @@ genButton.addEventListener('click', function(e){
      e.preventDefault();
      generatePassword();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-///////////////solution 1 without the use of checkboxes and input
-/*
-function generatePassword(){ 
-  //stored characters
-  let uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  let lowercase = 'abcdefghijklmnopqrstuvwxyz';
-  let numbers = '0123456789';
-  let specials = '!@#$%^&*()-={}~`<>?';
-  
-  //initialize password array
-  let passwordArray1 = [];
-
-  //variable holds random generated element from array
-  let randomNum;
-
-  //variable holds random generated number from string
-  let randomNum2;
-
-  //loop through the array given
-  for (let i = 0; i < 15; i++) {
-    randomNum = Math.floor(Math.random() * 4);//generate random character from 4 variables
-
-    //switch statement will determine which numbers are push through
-      switch(randomNum) {
-        case 1:
-        randomNum2 = Math.floor(Math.random() * 12);//random 12 characters are generated and held in randomNum2 variable
-        passwordArray1.push(uppercase[randomNum2]);
-        break;
-
-        case 2:
-        randomNum2 = Math.floor(Math.random() * 12);
-        passwordArray1.push(lowercase[randomNum2]);
-        break;
-
-        case 3: 
-        randomNumber2 = Math.floor(Math.random() * 12);
-        passwordArray1.push(numbers[randomNum2]);
-        break;
-
-        case 4: 
-        randomNumber2 = Math.floor(Math.random() * 12);
-        passwordArray1.push(specials[randomNum2]);
-        default:
-      }
-    }
-  
-    //Now we have a password Array, we need to make this into a stirng and get it in the DOM
-    var button = document.querySelector('#displayPassword');
-      button.innerText = passwordArray1.join('');
-}
-
-genButton = document.querySelector('.click');
-genButton.addEventListener('click', function(e){
-     e.preventDefault();
-     generatePassword();
-});
-*/
