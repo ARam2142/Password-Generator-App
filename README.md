548c103da67da1274b481f5bc01e9239d1174e5d
Andres Ramirez <andresaaramirez@aol.com>
2020-07-12 21:10:58 -0400

    i have updated my readme and made spacing changes in html and js

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Password-Generator-App
-Type in a password of your choice
+This application will allow you the(user)to generate a random password of your choice. The password generator was created with HTML,CSS bootstrap and Javascript. You will be given the option to create a password with the options of length and characters. You can include uppercase, lowercase, symbols or numbers in the password. You can select the length but must be between 8 and 128 characters long.
+
+
--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+//////////////////
+/*Please use at your own risk, this password generator is still in development
+you will not be able to select a length of your choice. You must check off the check boxes
+in order to generate password. Thank you and apologies for the inconvience.
+*/
+//////////////////
+
+//get elements by id
+const inputUpper = document.getElementById('uppercheck');
+const inputLower = document.getElementById('lowercheck');
+const inputSymbol = document.getElementById('symcheck');
+const inputNumber = document.getElementById('numcheck');
+const inputLength = document.getElementById('passwordlength');
 
 function generatePassword(){ 
   //stored characters
@@ -6,11 +19,8 @@ function generatePassword(){
   let numbers = '0123456789';
   let specials = '!@#$%^&*()-={}~`<>?';
 
-  //get elements by id
-  const inputUpper = document.getElementById('uppercheck');
-  const inputLower= document.getElementById('lowercheck');
-  const inputSymbol = document.getElementById('symcheck');
-  const inputNumber = document.getElementById('numcheck');
+  //set password length
+  let passlength = ''
 
   //initialize password array
   let passwordArray = [];
@@ -21,36 +31,32 @@ function generatePassword(){
   //variable holds random generated number from string
   let randomNum2;
 
-  //loop through the array given
-  for (let i = 0; i < 12; i++) {
-    randomNum = Math.floor(Math.random() * 4);//generate random character from 4 variabl
-    //switch statement will determine which numbers are push through
-      if(inputUpper == false && inputLower == false && inputSymbol == false && inputNumber == false) {
-        alert('please choose an option before hitting the button')
-        return false;
+  //loop through the values of the characters
+  for (let i = 0; i < 10; i++) {
+    randomNum = Math.floor(Math.random() * 4);//generate random character from 4 variab
+      
+        if(inputUpper.checked) {
+          randomNum2 = Math.floor(Math.random() * 21);
+          passwordArray.push(uppercase[randomNum2]);
+        }
+        if(inputLower.checked) {
+          randomNum2 = Math.floor(Math.random() * 21);
+          passwordArray.push(lowercase[randomNum2]);
+        } 
+        if(inputSymbol.checked) {
+          randomNum2 = Math.floor(Math.random() * 15);
+          passwordArray.push(numbers[randomNum2]);
+        } 
+        if(inputNumber.checked) {
+          randomNum2 = Math.floor(Math.random() * 10);
+          passwordArray.push(specials[randomNum2]);
+        }
       }
-      if(inputUpper.checked) {
-        randomNum2 = Math.floor(Math.random() * 5);//random 12 characters are generated and held in randomNum2 variable
-        passwordArray.push(uppercase[randomNum2]);
-      }
-      if(inputLower.checked) {
-        randomNum2 = Math.floor(Math.random() * 9);
-        passwordArray.push(lowercase[randomNum2]);
-      }
-
-      if(inputSymbol.checked) {
-        randomNum2 = Math.floor(Math.random() * 4);
-        passwordArray.push(specials[randomNum2]);
-      }
-      if(inputNumber.checked) {
-        randomNum2 = Math.floor(Math.random() * 10);
-        passwordArray.push(numbers[randomNum2]);
-      }
-    }
+    
     //Now we have a password Array, we need to make this into a stirng and get it in the DOM
     const button = document.querySelector('#displayPassword');
     button.innerText = passwordArray.join('');
-
+    
 }
 
 genButton = document.querySelector('.click');
@@ -58,40 +64,6 @@ genButton.addEventListener('click', function(e){
      e.preventDefault();
      generatePassword();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
